19866edc951e2f1bbd87bb5928fb80c853501f9d
angelospaulo <angelospaulo@gmail.com>
2023-05-01 11:41:49 -0300

    c

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ContaTerminal{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Por favor, digite o número da Conta!");
+
+        int numero = sc.nextInt();
+
+        System.out.println();
+        
+        System.out.println("Por favor, digite o número da Agência!");
+
+        String agencia = sc.
+
+
+
+        System.out.printf("Agência é % , conta é %", agencia + numero);
+        System.out.println();
+    }
+}
